25773deb55066beebb8b4e55c363f6fa6085e351
mwan9ilwa <mwangilwa23@gmail.com>
2022-10-21 19:13:36 +0200

    hide therapy jobs

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -714,7 +714,7 @@
         <div class="top-bar-boxed h-[70px] md:h-[65px] z-[51] border-b border-white/[0.08] mt-12 md:mt-0 -mx-3 sm:-mx-8 md:-mx-0 px-3 md:border-b-0 relative md:fixed md:inset-x-0 md:top-0 sm:px-8 md:px-10 md:pt-10 md:bg-gradient-to-b md:from-slate-100 md:to-transparent dark:md:from-darkmode-700">
             <div class="h-full flex items-center">
                 <!-- BEGIN: Logo -->
-                <a href="" class="logo -intro-x hidden md:flex xl:w-[180px] block">
+                <a href="{{ route('welcome')}}" class="logo -intro-x hidden md:flex xl:w-[180px] block">
                     <img style="border-radius: 100%" src="uploads/sites/304/2022/06/logos.svg" class="logo__image w-6" alt="" loading="lazy" />
                     {{-- <img alt="Midone - HTML Admin Template" class="logo__image w-6" src="dist/images/logo.svg"> --}}
                     <span class="logo__text text-white text-lg ml-3"> Nsansa </span> 
