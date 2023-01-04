fa1a4032ddfdbc6a5e8cf6eb42c2045893428362
Br3-mah <nyeleti.bremah@gmail.com>
2023-01-04 09:17:23 +0200

    500 error Fix | Appointment Index

--- resources/views/page/appointments/index.blade.php
+++ resources/views/page/appointments/index.blade.php
@@ -50,7 +50,7 @@
                 <a href="{{ route('appointment.create', ['type' => 'video']) }}" class="btn btn-primary w-full mt-2"> <i class="w-4 h-4 mr-2" data-lucide="video"></i> Add New Video Call Appointment </a>
                 <a href="{{ route('appointment.create', ['type' => 'phone']) }}" class="btn btn-primary w-full mt-2"> <i class="w-4 h-4 mr-2" data-lucide="phone-call"></i> Add New Phone Call Appointment </a>
                 <div class="border-t border-b border-slate-200/60 dark:border-darkmode-400 mt-6 mb-5 py-3" id="calendar-events">
-                    @if (!empty($appointments))
+                    @if (!empty($appointments->toArray()))
                     @forelse ($appointments as $appointment)
                     <div class="relative items-center flex transition rounded-md p-2 duration-300 ease-in-out hover:bg-slate-100 dark:hover:bg-darkmode-400">
                         <div class="event p-3 -mx-3 {{  $appointment->status == 0 ? 'disabled bg-slate-200 italic' : 'cursor-pointer' }} flex items-center">
@@ -94,7 +94,7 @@
                     @endforelse
                     @endif
 
-                    @if (!empty($incoming_appointments))
+                    @if(!empty($incoming_appointments->toArray()))
                     @forelse ($incoming_appointments as $app)
                     <div class="relative items-center flex transition rounded-md p-2">
                         <div class="event p-3 -mx-3 {{  $app->appointment->status == 0 ? 'disabled bg-slate-200 italic' : 'cursor-pointer' }} flex items-center">
