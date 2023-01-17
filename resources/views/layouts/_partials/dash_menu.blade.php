844cc059e0bf28796e0d0a174136e78bb204042a
Br3-mah <nyeleti.bremah@gmail.com>
2023-01-17 16:59:19 +0200

    added comission and department settings section 2

--- resources/views/layouts/_partials/dash_menu.blade.php
+++ resources/views/layouts/_partials/dash_menu.blade.php
@@ -3,7 +3,7 @@
 <div id="onChatMenu" class="top-bar-boxed h-[70px] md:h-[65px] z-[51] border-b border-white/[0.08] mt-12 md:mt-0 -mx-3 sm:-mx-8 md:-mx-0 px-3 md:border-b-0 relative md:fixed md:inset-x-0 md:top-0 sm:px-8 md:px-10 md:pt-10 md:bg-gradient-to-b md:from-slate-100 md:to-transparent dark:md:from-darkmode-700">
     <div class="h-full flex items-center">
         <!-- BEGIN: Logo -->
-        <a href="{{ route('welcome')}}" class="logo -intro-x hidden md:flex xl:w-[180px] block">
+        <a href="{{ route('home')}}" class="logo -intro-x hidden md:flex xl:w-[180px] block">
             <img style="border-radius: 100%" src="{{ asset('uploads/sites/304/2022/06/logos.svg') }}" class="logo__image w-6" alt="" loading="lazy" />
             {{-- <img alt="Midone - HTML Admin Template" class="logo__image w-6" src="dist/images/logo.svg"> --}}
             <span class="logo__text text-white text-lg ml-3"> Nsansa Wellness </span> 
@@ -12,7 +12,7 @@
         <!-- BEGIN: Breadcrumb -->
         <nav aria-label="breadcrumb" class="-intro-x h-[45px] mr-auto">
             <ol class="breadcrumb breadcrumb-light">
-                <li class="breadcrumb-item"><a href="#">Nsansa Wellness</a></li>
+                <li class="breadcrumb-item"><a href="{{ route('home') }}">Nsansa Wellness</a></li>
                 <li class="capitalize breadcrumb-item active" aria-current="page">
                     {{ str_replace('-', ' ', Request::route()->uri)  }}
                 </li>
