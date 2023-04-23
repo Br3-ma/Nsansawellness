a44e064834f4c3273b8417e7851e3250d049e939
br3-mah <nyeleti.bremah@gmail.com>
2023-04-23 21:32:30 +0200

    Site Rating

--- config/livewire.php
+++ config/livewire.php
@@ -68,7 +68,7 @@ return [
     |
     */
 
-    'app_url' =>  env('APP_URL', 'http://localhost/nsansaWellness'),
+    'app_url' =>  env('APP_URL', 'http://localhost/nsansawellness/'),
 
     /*
     |--------------------------------------------------------------------------
