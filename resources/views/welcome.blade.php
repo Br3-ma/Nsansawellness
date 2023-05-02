262d51aae9f3af8a43502d4c797a8e1c461985c6
br3-mah <nyeleti.bremah@gmail.com>
2023-05-02 14:00:56 +0200

    values and missions

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -230,7 +230,7 @@
 														<li class="elementor-icon-list-item">
 															<span class="elementor-icon-list-icon">
 																<i aria-hidden="true" class="fas fa-check-circle"></i> </span>
-															<span class="elementor-icon-list-text">Empolyee Counseling Services</span>
+															<span class="elementor-icon-list-text">Employee Counseling Services</span>
 														</li>
 														<li class="elementor-icon-list-item">
 															<span class="elementor-icon-list-icon">
