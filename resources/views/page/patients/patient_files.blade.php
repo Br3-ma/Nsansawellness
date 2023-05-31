2e56d0d22044d5082f916e76e83a6c9e3417b43c
br3-mah <nyeleti.bremah@gmail.com>
2023-05-31 11:45:38 +0200

    minor changes

--- resources/views/page/patients/patient_files.blade.php
+++ resources/views/page/patients/patient_files.blade.php
@@ -218,11 +218,11 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/selectize.js/0.12.6/js/standalone/selectize.min.js" integrity="sha256-+C0A5Ilqmu4QcSPxrlGpaZxJ04VjsRjKu+G82kl5UJk=" crossorigin="anonymous"></script>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/selectize.js/0.12.6/css/selectize.bootstrap3.min.css" integrity="sha256-ze/OEYGcFbPRmvCnrSeKbRTtjG4vGLHXgOqsyLFTRjg=" crossorigin="anonymous" />
 <script>
-  $(document).ready(function () {
-      $('select').selectize({
-          sortField: 'text'
-      });
-  });
+//   $(document).ready(function () {
+//       $('select').selectize({
+//           sortField: 'text'
+//       });
+//   });
     var counselor_id = 0;
     var patient_id = 0;
 
