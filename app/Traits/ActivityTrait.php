7aa3dffba84fe5ec038d4fca88079a5f04354904
br3-mah <nyeleti.bremah@gmail.com>
2023-07-21 10:48:06 +0200

    Bugs | Activity Index

--- app/Traits/ActivityTrait.php
+++ app/Traits/ActivityTrait.php
@@ -2,29 +2,21 @@
 
 namespace App\Traits;
 
-use App\Models\Activity;
 use App\Models\PatientActivity;
 
 trait ActivityTrait {
-    public $activities, $patient_activities;
-
-    public function __construct(Activity $activities, PatientActivity $patientActivity)
-    {
-        $this->activities = $activities;
-        $this->patient_activities = $patientActivity;
-    }
 
     public function getActivitiesForCounselor(){
-        return $this->patient_activities->where('counselor_id', auth()->user()->id)
+        return PatientActivity::where('counselor_id', auth()->user()->id)
             ->with('activities.users')->with('activities.counselor')->get();
     }
 
     public function getActivitiesForPatient(){
-        return $this->patient_activities->where('user_id', auth()->user()->id)
+        return PatientActivity::where('user_id', auth()->user()->id)
         ->with('activities.users')->with('activities.counselor')->get();  
     }
     public function getActivitiesForPatientApi($id){
-        return $this->patient_activities->where('user_id', $id)
+        return PatientActivity::where('user_id', $id)
         ->with('activities.users')->with('activities.counselor')->get();  
     }
 
