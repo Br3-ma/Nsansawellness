e6dd485814a2aea019784e24fec31808a1ba3056
br3-mah <nyeleti.bremah@gmail.com>
2023-07-21 12:34:36 +0200

    Respective patient questionnaire views

--- app/Traits/CounselorTrait.php
+++ app/Traits/CounselorTrait.php
@@ -2,12 +2,14 @@
 
 namespace App\Traits;
 
+use App\Models\User;
+
 trait CounselorTrait {
     use ChatTrait;
 
     public function getMyCounselor($u){
         $my_active_chat = $this->active_chat_info($u);
-        $counselor = $this->user->role('counselor')->where('id', $my_active_chat->sender_id)->get();
+        $counselor = User::role('counselor')->where('id', $my_active_chat->sender_id)->get();
         return $counselor;
     }
 
