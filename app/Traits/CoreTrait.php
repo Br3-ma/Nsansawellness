181450fc58ba72b1864043c1dda2bc324672c4de
br3-mah <nyeleti.bremah@gmail.com>
2023-07-21 12:15:58 +0200

    Respective patient questionnaire views

--- app/Traits/CoreTrait.php
+++ app/Traits/CoreTrait.php
@@ -3,24 +3,9 @@
 namespace App\Traits;
 
 use App\Models\Appointment;
-use App\Models\Billing;
 use App\Models\Chat;
-use App\Models\PatientFile;
-use App\Models\User;
 
 trait CoreTrait {
-    public $users, $pf, $appointment, $chat, $billing;
-
-        public function __construct(User $users, PatientFile $pf, Appointment $app, Chat $chat, Billing $billing)
-        {
-            $this->middleware('auth');
-            $this->appointment = $app;
-            $this->users = $users;
-            $this->pf = $pf;
-            $this->billing = $billing;
-            $this->chat = $chat;
-            $this->middleware(['auth', 'verified']);
-        }
 
         public function my_role(){            
             return auth()->user()->roles->pluck('name')->first();
@@ -28,11 +13,11 @@ trait CoreTrait {
 
         public function get_my_chats(){
             if($this->my_role() == 'patient'){
-                $chats = $this->chat->where('receiver_id', auth()->user()->id)
+                $chats = Chat::where('receiver_id', auth()->user()->id)
                 ->where('status', 1)
                 ->with(['sender', 'receiver'])->get();
             }else{
-                $chats = $this->chat->where('sender_id', auth()->user()->id)
+                $chats = Chat::where('sender_id', auth()->user()->id)
                 ->where('status', 1)
                 ->with(['sender', 'receiver'])->get();
             }
@@ -41,9 +26,7 @@ trait CoreTrait {
         }
 
         public function get_my_appointments(){
-            $data = $this->appointment->where('user_id', auth()->user()->id)->get();
-            // $incoming_appointments = UserAppointment::with('appointment')->where('guest_id', Auth::user()->id)->get();
-            
+            $data = Appointment::where('user_id', auth()->user()->id)->get();
             return $data;
         }
 
