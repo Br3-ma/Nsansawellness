b30e98d532cc047f6a2b20654cf636a0e37a8bf2
br3-mah <nyeleti.bremah@gmail.com>
2023-07-21 12:12:40 +0200

    Respective patient questionnaire views

--- app/Traits/CounselorTrait.php
+++ app/Traits/CounselorTrait.php
@@ -2,28 +2,8 @@
 
 namespace App\Traits;
 
-use App\Models\AssignCounselor;
-use Illuminate\Http\Request;
-use App\Models\Appointment;
-use App\Models\PatientFile;
-use App\Models\User;
-
 trait CounselorTrait {
     use ChatTrait;
-    public $users, $u, $pf, $ac, $appointment;
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct(User $users, PatientFile $pf, Appointment $app, AssignCounselor $ac)
-    {
-        $this->middleware('auth');
-        $this->appointment = $app;
-        $this->user = $users;
-        $this->pf = $pf;
-        $this->ac = $ac;
-    }
 
     public function getMyCounselor($u){
         $my_active_chat = $this->active_chat_info($u);
