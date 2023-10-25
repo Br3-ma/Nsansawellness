f9ef8d8dd93be35002fe1a1611284207ba4349a3
unknown <theegreenweb@gmail.com>
2023-10-25 14:41:49 +0200

    fix activities

--- resources/views/page/activity/index.blade.php
+++ resources/views/page/activity/index.blade.php
@@ -92,7 +92,9 @@
                                 <td>
                                     <div class="text-slate-500 text-xs whitespace-nowrap mt-0.5">
                                         @forelse (App\Models\Activity::assignedPatients($act->activities->id) as $user)
+                                            @if ($user)
                                             {{ $user->users->fname.' '.$user->users->lname }}
+                                            @endif
                                         @empty
                                             
                                         @endforelse
